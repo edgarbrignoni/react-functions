b9a618954894cbad9bebad1984fb5b97955e84fb
Edgar Brignoni <edgarbrignoni@icloud.com>
2018-07-26 20:27:10 +0000

    add webhook

--- public/bundle.js
+++ public/bundle.js
@@ -62,7 +62,7 @@
 /******/ 	}
 /******/
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "fca2adf19799281bcbff"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "5d50ebf250344aa3f25c"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
@@ -752,7 +752,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/";
+/******/ 	__webpack_require__.p = "/public";
 /******/
 /******/ 	// __webpack_hash__
 /******/ 	__webpack_require__.h = function() { return hotCurrentHash; };
